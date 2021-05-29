6bcc775851729c502eebb0ea3ea28b4910fd6fad
agarwalpreetansh <79320209+agarwalpreetansh@users.noreply.github.com>
2021-05-29 13:22:23 +0530

    Add files via upload

---
+++ Particle.js
@@ -0,0 +1,36 @@
+class Particle
+{
+	constructor(x,y,r)
+	{
+		var options={
+			
+			restitution:0.4
+			
+			
+
+			
+			}
+		this.x=x;
+		this.y=y;
+		this.r=r
+		this.body=Bodies.circle(this.x, this.y, this.r/2, options)
+		this.color=color(random(0,255),random(0,255),random(0,255))
+        World.add(world, this.body);
+
+	}
+	display()
+	{
+			
+			var paperpos=this.body.position;		
+
+			push()
+			translate(paperpos.x, paperpos.y);
+			rectMode(CENTER)
+			strokeWeight(3);
+			fill(this.color)
+			ellipse(0,0,this.r, this.r);
+			pop()   
+			
+	}
+
+}
